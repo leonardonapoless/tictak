583e7e706af397c518bb48cdecdba335cfdb4294
Leonardo Nápoles <158120970+leonardonapoless@users.noreply.github.com>
2025-08-05 18:17:27 -0300

    Initial Commit

---
+++ tictak/tictakApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictakApp.swift
+//  tictak
+//
+//  Created by Leonardo Nápoles on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct tictakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
