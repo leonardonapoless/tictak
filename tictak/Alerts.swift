c52988724870e6f70fd187361d2c7104bb67e76a
Leonardo <158120970+leonardonapoless@users.noreply.github.com>
2025-08-07 23:16:34 -0300

    Initial commit

---
+++ tictak/Alerts.swift
@@ -0,0 +1,8 @@
+//
+//  Alerts.swift
+//  tictak
+//
+//  Created by Leonardo Nápoles on 8/7/25.
+//
+
+import Foundation
---
+++ tictak/GameViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GameViewModel.swift
+//  tictak
+//
+//  Created by Leonardo Nápoles on 8/7/25.
+//
+
+import Foundation
